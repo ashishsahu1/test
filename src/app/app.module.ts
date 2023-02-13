c8aefdaa0275b8627648facb7375715a54fe3efb
Ashish Sahu <ashishsahu10428@gmail.com>
2023-02-13 21:26:56 +0530

    DP added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './Pages/home/home.component';
 import { NavbarComponent } from './Components/navbar/navbar.component';
+import { AboutSectionComponent } from './Components/about-section/about-section.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    AboutSectionComponent
   ],
   imports: [
     BrowserModule
