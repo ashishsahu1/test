ae3983b544f8238cb8e00546208e4656ab9df800
Ashish Sahu <ashishsahu10428@gmail.com>
2023-02-15 02:54:37 +0530

    Stack list implementation complete

--- src/app/Components/list-section/list-section.component.ts
+++ src/app/Components/list-section/list-section.component.ts
@@ -6,74 +6,271 @@ import { Component } from '@angular/core';
   styleUrls: ['./list-section.component.css']
 })
 export class ListSectionComponent {
+  show = 30
+  red:string = "#FFC7C7"
+  blue:string = "#C7EBFF"
+  green:string = "#CBF2EC"
+  yellow:string = "#FFF9C7"
+  pink:string = "#E7D5FF"
+
   stack_list = [
     {
+      // Green
       txt:"Algorithm",
-      icn:"devicon-sourcetree-original"
+      icn:"devicon-sourcetree-original colored",
+      color_code:this.green
     },
     {
-      txt:"Algorithm",
-      icn:"devicon-sourcetree-original"
+      // Red
+      txt:"Angular JS",
+      icn:"devicon-angularjs-plain colored",
+      color_code:this.red
     },
     {
-      txt:"Algorithm",
-      icn:"devicon-sourcetree-original"
+      // Blue
+      txt:"React JS",
+      icn:"devicon-react-original colored",
+      color_code:this.blue
     },
     {
-      txt:"Algorithm",
-      icn:"devicon-sourcetree-original"
+      // Yellow
+      txt:"Javascript",
+      icn:"devicon-javascript-plain colored",
+      color_code:this.yellow
     },
     {
-      txt:"Algorithm",
-      icn:"devicon-sourcetree-original"
+      txt:"HTML 5",
+      icn:"devicon-html5-plain colored",
+      color_code:this.red
     },
     {
-      txt:"Algorithm",
-      icn:"devicon-sourcetree-original"
+      txt:"CSS",
+      icn:"devicon-css3-plain colored",
+      color_code:this.blue
     },
     {
-      txt:"Algorithm",
-      icn:"devicon-sourcetree-original"
+      txt:"SCSS",
+      icn:"devicon-sass-original colored",
+      color_code:this.pink
     },
     {
-      txt:"Algorithm",
-      icn:"devicon-sourcetree-original"
+      txt:"Bootstrap",
+      icn:"devicon-bootstrap-plain-wordmark colored",
+      color_code:this.pink
     },
     {
-      txt:"Algorithm",
-      icn:"devicon-sourcetree-original"
+      txt:"Bulma",
+      icn:"devicon-bulma-plain colored",
+      color_code:this.green
     },
     {
-      txt:"Algorithm",
-      icn:"devicon-sourcetree-original"
+      txt:"Material UI",
+      icn:"devicon-materialui-plain colored",
+      color_code:this.blue
     },
     {
-      txt:"Algorithm",
-      icn:"devicon-sourcetree-original"
+      txt:"MongoDB",
+      icn:"devicon-mongodb-plain colored",
+      color_code:this.green
     },
     {
-      txt:"Algorithm",
-      icn:"devicon-sourcetree-original"
+      txt:"Express JS",
+      icn:"devicon-express-original colored",
+      color_code:this.pink
     },
     {
-      txt:"Algorithm",
-      icn:"devicon-sourcetree-original"
+      txt:"Node JS",
+      icn:"devicon-nodejs-plain colored",
+      color_code:this.green
     },
     {
-      txt:"Algorithm",
-      icn:"devicon-sourcetree-original"
+      txt:"NodeWebKit",
+      icn:"devicon-nodewebkit-plain colored",
+      color_code:this.blue
     },
     {
-      txt:"Algorithm",
-      icn:"devicon-sourcetree-original"
+      txt:"DotNet",
+      icn:"devicon-dot-net-plain colored",
+      color_code:this.blue
     },
     {
-      txt:"Algorithm",
-      icn:"devicon-sourcetree-original"
+      txt:".NET Core",
+      icn:"devicon-dotnetcore-plain colored",
+      color_code:this.pink
     },
+    {
+      txt:"VS Code",
+      icn:"devicon-vscode-plain colored",
+      color_code:this.blue
+    },
+    {
+      txt:"Visual Studio",
+      icn:"devicon-visualstudio-plain colored",
+      color_code:this.pink
+    },
+    {
+      txt:"FastAPI",
+      icn:"devicon-fastapi-plain colored",
+      color_code:this.green
+    },
+    {
+      txt:"MySQL",
+      icn:"devicon-mysql-plain colored",
+      color_code:this.pink
+    },
+    {
+      txt:"Postgre SQL",
+      icn:"devicon-postgresql-plain colored",
+      color_code:this.blue
+    },
+    {
+      txt:"Github",
+      icn:"devicon-github-original colored",
+      color_code:this.green
+    },
+    {
+      txt:"GIT control",
+      icn:"devicon-git-plain colored",
+      color_code:this.red
+    },
+    {
+      txt:"Azure",
+      icn:"devicon-azure-plain colored",
+      color_code:this.blue
+    },
+    {
+      txt:"AWS",
+      icn:"devicon-amazonwebservices-original colored",
+      color_code:this.yellow
+    },
+    {
+      txt:"C++",
+      icn:"devicon-cplusplus-plain colored",
+      color_code:this.pink
+    },
+    {
+      txt:"Figma",
+      icn:"devicon-figma-plain colored",
+      color_code:this.red
+    },
+    {
+      txt:"DevIcon",
+      icn:"devicon-devicon-plain colored",
+      color_code:this.green
+    },
+    {
+      txt:"GCP",
+      icn:"devicon-googlecloud-plain colored",
+      color_code:this.blue
+    },
+    {
+      txt:"Python",
+      icn:"devicon-python-plain colored",
+      color_code:this.yellow
+    },
+    {
+      txt:"Jupyter",
+      icn:"devicon-jupyter-plain colored",
+      color_code:this.red
+    },
+    {
+      txt:"Pandas",
+      icn:"devicon-pandas-original colored",
+      color_code:this.pink
+    },
+    {
+      txt:"PyTorch",
+      icn:"devicon-pytorch-original colored",
+      color_code:this.red
+    },
+    {
+      txt:"Numpy",
+      icn:"devicon-numpy-original colored",
+      color_code:this.blue
+    },
+    {
+      txt:"TensorFlow",
+      icn:"devicon-tensorflow-original colored",
+      color_code:this.red
+    },
+    {
+      txt:"Anaconda",
+      icn:"devicon-anaconda-original colored",
+      color_code:this.green
+    },
+    {
+      txt:"Android Studio",
+      icn:"devicon-androidstudio-plain colored",
+      color_code:this.blue
+    },
+    {
+      txt:"Android",
+      icn:"devicon-android-plain colored",
+      color_code:this.green
+    },
+    {
+      txt:"Canva",
+      icn:"devicon-canva-original colored",
+      color_code:this.blue
+    },
+    {
+      txt:"Chrome",
+      icn:"devicon-chrome-plain colored",
+      color_code:this.red
+    },
+    {
+      txt:"C Sharp",
+      icn:"devicon-csharp-plain colored",
+      color_code:this.pink
+    },
+    {
+      txt:"Dart",
+      icn:"devicon-dart-plain colored",
+      color_code:this.blue
+    },
+    {
+      txt:"Flask",
+      icn:"devicon-flask-original colored",
+      color_code:this.red
+    },
+    {
+      txt:"Fire Base",
+      icn:"devicon-firebase-plain-wordmark colored",
+      color_code:this.red
+    },
+    {
+      txt:"Flutter",
+      icn:"devicon-flutter-plain colored",
+      color_code:this.blue
+    },
+    {
+      txt:"Heroku",
+      icn:"devicon-heroku-plain-wordmark colored",
+      color_code:this.pink
+    },
+    {
+      txt:"Flask",
+      icn:"devicon-flask-original colored",
+      color_code:this.red
+    },
+    {
+      txt:"Typescript",
+      icn:"devicon-typescript-plain colored",
+      color_code:this.blue
+    },
+    {
+      txt:"Flask",
+      icn:"devicon-flask-original colored",
+      color_code:this.red
+    },
+    
   ]
 
   constructor(){
     console.log(this.stack_list)
   }
+
+  increaseShow() {
+   this.show += 20; 
+ }
 }
